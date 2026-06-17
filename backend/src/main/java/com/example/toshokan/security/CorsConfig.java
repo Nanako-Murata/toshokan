06fa480fc08ab1849961e495422b892c45e600a7
Nanako-Murata <mokalate.nanako@gmail.com>
2026-06-17 11:04:02 +0900

    c

--- backend/src/main/java/com/example/toshokan/security/CorsConfig.java
+++ backend/src/main/java/com/example/toshokan/security/CorsConfig.java
@@ -1,13 +1,15 @@
 package com.example.toshokan.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer{
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:5173") // Viteのデフォルトポート
+	                .allowedOrigins("http://localhost:5173", "https://toshokan-frontend.onrender.com") // Viteのデフォルトポート
 	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                .allowedHeaders("*")
 	                .allowCredentials(true);
